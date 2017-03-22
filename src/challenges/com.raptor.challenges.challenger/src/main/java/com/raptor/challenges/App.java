cb0e3ca8ee168e76ccc25c538e2b3f404d435066
Andy Gardner <gardnerandy@gmail.com>
2017-03-21 23:03:35 -0500

    Updating Pom to be 1.8 compatible and fixing test suite

--- src/challenges/com.raptor.challenges.challenger/src/main/java/com/raptor/challenges/App.java
+++ src/challenges/com.raptor.challenges.challenger/src/main/java/com/raptor/challenges/App.java
@@ -8,6 +8,56 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        if(args.length == 0)
+        {
+        	usage();
+        	return;
+        }
+        
+        for(int i = 0; i < args.length; i++)
+        {
+        	switch(args[i])
+        	{
+        		case("-factorial") :
+        			if(i != args.length-1)
+        			{
+        				i++;
+        				try
+        				{
+        					
+        					Long facParam = Long.parseLong(args[i]);
+        					System.out.println(Long.toString(factorialChallenge(facParam)));	
+        				}
+        				catch(NumberFormatException e)
+        				{
+        					e.printStackTrace();
+        					usage();
+        					return;
+        				}
+        			}
+        		break;
+        	}
+        }
+    }
+    
+    public static void usage()
+    {
+    	StringBuffer buff = new StringBuffer();
+    	buff.append("Welcome to the challenger.  Acceptable arguments as follows:");
+    	buff.append("\n\n-?:\t\tPrint Usage");
+    	buff.append("\n-factorial <value> :\tPrint Factorial value of parameter.");
+    	System.out.println(buff.toString());
+    }
+    
+    public static long factorialChallenge(long val)
+    {
+    	if(val > 1)
+    	{
+    		return val * factorialChallenge(val - 1);
+    	}
+    	else
+    	{
+    		return val;
+    	}
     }
 }
